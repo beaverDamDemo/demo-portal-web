4d5a55cf1f4c613b605ece0dd264e184a6fcf332
beaverDamDemo <zdenevi@gmail.com>
2025-09-05 23:14:59 +0200

    added global loader. Added view transitions that do not work in current firefox

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,42 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { Theme, ThemeService } from './services/theme-service';
 import { AuthService } from './services/auth';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatDividerModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, MatToolbarModule, MatDividerModule, MatButtonModule, MatIconModule, MatProgressBarModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  protected readonly title = signal('web-demo-portal');
+  protected readonly title = signal('Demo Portal');
   currentTheme: Theme = 'light';
   private readonly themeService = inject(ThemeService);
   public readonly authService = inject(AuthService);
   currentYear = new Date().getFullYear();
+  loading = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.loading = false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.themeService.theme$.subscribe(theme => {
