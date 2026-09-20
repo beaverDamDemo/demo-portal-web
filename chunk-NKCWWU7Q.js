e033741f57b8c43c4e632d3d1ec25cc09e782f8c
beaverDamDemo <zdenevi@gmail.com>
2026-09-20 12:04:30 +0200

    Auto-generated commit

---
+++ chunk-NKCWWU7Q.js
@@ -0,0 +1,1 @@
+var e={LUFTHANSA_CLIENT_KEY:"undefined",LUFTHANSA_CLIENT_SECRET:"undefined",API_NINJAS_API_KEY:"zorWh15QT8tYmNrM6jihLw==8TFrmrjpA0040L1i",AVIATION_STACK_API_KEY:"4961ede6b8ed9d3994f2fa970b8c2310",API_URL:"https://beaver-dam-demo-app-hub-api.onrender.com/demo-portal/",appVersion:"0.0.1",buildDate:"Sep 20, 2026, 12:04",production:!0};export{e as a};
