94aff4565593b05ecddc23071918e93fced534ea
beaverDamDemo <zdenevi@gmail.com>
2026-09-20 11:52:06 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
     LUFTHANSA_CLIENT_SECRET: 'undefined',
     API_NINJAS_API_KEY: 'zorWh15QT8tYmNrM6jihLw==8TFrmrjpA0040L1i',
     AVIATION_STACK_API_KEY: '4961ede6b8ed9d3994f2fa970b8c2310',
-    API_URL: 'http://localhost:3000/demo-portal',
+    API_URL: 'https://beaver-dam-demo-app-hub-api.onrender.com',
     appVersion: '0.0.1',
-    buildDate: 'Sep 13, 2026, 16:11',
-    production: false,
+    buildDate: 'Sep 20, 2026, 11:40',
+    production: true,
   };
